3ff8ded7808e2a30f3f08c131c487a182d3338ae
Max <maxtarasevich@gmail.com>
2021-09-04 23:30:40 +0300

    feat: add guessing-game methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0
+        this.max = 0
+        this.rez = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.rez = Math.round((this.min + this.max)/2)
+        return this.rez
     }
 
     lower() {
-
+        this.max = this.rez
     }
 
     greater() {
-
+        this.min = this.rez
     }
 }
 
